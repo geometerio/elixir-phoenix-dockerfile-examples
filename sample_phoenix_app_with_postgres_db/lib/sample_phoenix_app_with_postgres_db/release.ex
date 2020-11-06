f88b63600026d4a5ccf1858d5d38f72d2497e3bd
Greg Woodward <woodward@alumni.stanford.edu>
2020-11-06 15:58:34 -0800

    Copied over from other repos

---
+++ sample_phoenix_app_with_postgres_db/lib/sample_phoenix_app_with_postgres_db/release.ex
@@ -0,0 +1,29 @@
+defmodule SamplePhoenixAppWithPostgresDB.Release do
+  @moduledoc """
+  Export functions to run in the context of Elixir releases.
+  """
+
+  @app :sample_phoenix_app_with_postgres_db
+
+  def migrate do
+    ensure_started()
+
+    for repo <- repos() do
+      {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
+    end
+  end
+
+  def rollback(repo, version) do
+    ensure_started()
+    {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
+  end
+
+  defp repos do
+    Application.load(@app)
+    Application.fetch_env!(@app, :ecto_repos)
+  end
+
+  defp ensure_started do
+    Application.ensure_all_started(:ssl)
+  end
+end
